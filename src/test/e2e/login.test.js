943e1042ee0522954f5557a22f7b841a931b3055
TJ von Mechow <tj@TJs-MacBook-Pro.local>
2021-08-27 11:44:15 -0400

    feat(e2e)[]: add selenium e2e test for email/password login flow

---
+++ src/test/e2e/login.test.js
@@ -0,0 +1,15 @@
+const webdriver = require('selenium-webdriver');
+const { expect } = require('chai')
+const chromeDriver = new webdriver.Builder().forBrowser('chrome').build();
+
+chromeDriver.navigate().to(`https://yellow-sand-098426110.azurestaticapps.net`)
+    .then(() => chromeDriver.findElement(webdriver.By.className(`MuiButtonBase-root`)).click())
+    .then(() => chromeDriver.findElements(webdriver.By.css(`li`)))
+    .then(elements => elements[1].click())
+    .then(() => chromeDriver.wait(webdriver.until.elementLocated(webdriver.By.id(`email`))))
+    .then(() => chromeDriver.findElement(webdriver.By.id(`email`)).sendKeys(`aj@thirteendelta.com`))
+    .then(() => chromeDriver.findElement(webdriver.By.id(`password`)).sendKeys(`Mu$t@rD1!`))
+    .then(() => chromeDriver.findElement(webdriver.By.id(`next`)).click())
+    .then(() => chromeDriver.wait(webdriver.until.elementLocated(webdriver.By.css(`h6`))))
+    .then(() => chromeDriver.findElement(webdriver.By.css(`h6`)).getText())
+    .then(text => expect(text).to.equal(`Welcome, A.J.`))
